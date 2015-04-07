7f5017a4cd2d0973b43f52f3504ad64c557c2621
Yasuaki KAWAKAMI <bougan1160@gmail.com>
2015-04-07 17:28:20 +0900

    [add] Add function of dividing schema:Performer

--- src/LFJBParser.php
+++ src/LFJBParser.php
@@ -6,9 +6,10 @@
  * Date: 4/5/15
  * Time: 10:17
  */
+
+namespace Bougan1160\LFJBParser;
 class LFJBParser
 {
-
 
     function __construct()
     {
@@ -19,16 +20,46 @@ class LFJBParser
         $defaultOptions = array();
     }
 
-    public function parsePerformer($performerJSON, $options)
+    public function parsePerformer($performerJSON, $options = array())
     {
-        if (isset($performerJSON['@type']) && $performerJSON['@type'] == "schema:Person") {
-            throw new Exception("Unknown schema");
+        if (!isset($performerJSON['@type']) || $performerJSON['@type'] != "schema:Person") {
+            throw new \Exception("Unknown schema");
         }
-
+        $names = $performerJSON['schema:name'];
+        $performers = array();
+        if (is_array($names)) {
+            foreach ($names as $name) {
+                $performers = array_merge($performers, $this->explodePerformerName($name));
+            }
+        } else {
+            $performers = $this->explodePerformerName($names);
+        }
+        if (count($performers) >= 2)
+            $performerJSON = $this->createPersonSchema($performers);
+        return $performerJSON;
     }
 
     public function parseDate($dateJSON, $options)
     {
 
+    }
+
+    private function createPersonSchema($personNames)
+    {
+        $persons = array();
+        foreach ($personNames as $personName) {
+            $persons[] = array(
+                "@type" => "schema:Person",
+                "schema:name" => $personName
+            );
+        }
+        return $persons;
+    }
+
+    private function explodePerformerName($name)
+    {
+        $name = preg_replace("/([（|(][^）|^)]+、.+[）|)])|([（|(][^）|^)]+／.+[）|)])|(、)|(／)/", "$1$2,", $name);
+        $name = preg_replace("/） |）/", "）,", $name);
+        return array_filter(explode(",", $name), "strlen");
     }
 }
--- unitTest/LFJBParserTest.php
+++ unitTest/LFJBParserTest.php
@@ -6,19 +6,53 @@
  * Date: 4/5/15
  * Time: 11:35
  */
-require dirname(dirname(dirname(__FILE__))) . '/vendor/autoload.php';
+use Bougan1160\LFJBParser\LFJBParser;
 
-class LFJBParserTest extends PHPUnit_Framework_TestCase
+class LFJBParserTest extends \PHPUnit_Framework_TestCase
 {
+    private $parser;
+    private $originalJSONLD;
+    private $parsedJSONLD;
 
-    public function tearUp()
+    public function setUp()
     {
         $this->parser = new LFJBParser();
+        $this->originalJSONLD = json_decode(file_get_contents("events-jsonld.jsonld"), true);
+        $this->parsedJSONLD = json_decode(file_get_contents("parsed-events-jsonld.jsonld"), true);
     }
 
-    public function testParsePerformer()
+    public function testParseSinglePerformer()
     {
-        $this->assertEquals(1, 1);
+        //http://lfjb.biwako-hall.or.jp/events/487/jsonld/
+        $performerData_Single = $this->originalJSONLD[0]['schema:performer'];
+        $expected = $this->parsedJSONLD[0]['schema:performer'];
+        $result = $this->parser->parsePerformer($performerData_Single);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testParseManyPerformer()
+    {
+        //http://lfjb.biwako-hall.or.jp/events/491/jsonld/
+        $performerData_Many = $this->originalJSONLD[14]['schema:performer'];
+        $result = $this->parser->parsePerformer($performerData_Many);
+        $expected = $this->parsedJSONLD[14]['schema:performer'];
+        $this->assertEquals($expected, $result);
+
+        //http://lfjb.biwako-hall.or.jp/events/503/jsonld/
+        $performerData_Many = $this->originalJSONLD[4]['schema:performer'];
+        $result = $this->parser->parsePerformer($performerData_Many);
+        $expected = $this->parsedJSONLD[4]['schema:performer'];
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @expectedException        Exception
+     * @expectedExceptionMessage Unknown schema
+     */
+    public function testParseUnknownTypePerformer()
+    {
+        $performerData = $this->originalJSONLD[0];
+        $this->parser->parsePerformer($performerData);
     }
 }
  
